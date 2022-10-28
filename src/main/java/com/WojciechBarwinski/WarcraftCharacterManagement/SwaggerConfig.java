2a5fa797c6c88655df8167fd2d2a481cc8e4e8ae
Wojciech Barwiński <thaazar@gmail.com>
2022-10-28 13:36:43 +0200

    add swagger to project

---
+++ src/main/java/com/WojciechBarwinski/WarcraftCharacterManagement/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.WojciechBarwinski.WarcraftCharacterManagement;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
+                .build();
+                //.apiInfo(apiInfo());
+    }
+
+
+    /*private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Java Hungry API")
+                .description("Java Hungry API's")
+                .version("1.0.0")
+                .termsOfServiceUrl("http://javahungry.blogspot.com")
+                .license("JavaHungry License")
+                .licenseUrl("http://javahungry.blogspot.com")
+                .build();
+    }*/
+}
+
